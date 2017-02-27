247a53ebc8f85d433736048806cd31a35e1067d9
Gautam Shah <gautamrshah@gmail.com>
2017-02-27 20:49:54 +0000

    Part 1 - Postcode validation unit test

--- src/main/java/com/postcode/PostCodeValidator.java
+++ src/main/java/com/postcode/PostCodeValidator.java
@@ -10,10 +10,18 @@ import java.util.regex.Pattern;
  *
  * http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
  */
+
 public class PostCodeValidator {
-    private final static String postCodePattern = "(GIR\\s0AA)|((([A-PR-UWYZ][0-9][0-9]?)|(([A-PR-UWYZ][A-HK-Y][0-9](?<!(BR|FY|HA|HD|HG|HR|HS|HX|JE|LD|SM|SR|WC|WN|ZE)[0-9])[0-9])|([A-PR-UWYZ][A-HK-Y](?<!AB|LL|SO)[0-9])|(WC[0-9][A-Z])|(([A-PR-UWYZ][0-9][A-HJKPSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))\\s[0-9][ABD-HJLNP-UW-Z]{2})";//"^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$";
+    /*
+     *  Used postcode regex from the verbose format provided in tasking
+     */
+    private final static String postCodePattern = "(GIR\\s0AA)|((([A-PR-UWYZ][0-9][0-9]?)|(([A-PR-UWYZ][A-HK-Y][0-9](?<!(BR|FY|HA|HD|HG|HR|HS|HX|JE|LD|SM|SR|WC|WN|ZE)[0-9])[0-9])|([A-PR-UWYZ][A-HK-Y](?<!AB|LL|SO)[0-9])|(WC[0-9][A-Z])|(([A-PR-UWYZ][0-9][A-HJKPSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))\\s[0-9][ABD-HJLNP-UW-Z]{2})";
     private final static Pattern pattern = Pattern.compile(postCodePattern, Pattern.CASE_INSENSITIVE);
 
+    /*
+     *  This code is to cater Part 1 of Postcode validation
+     *  link - https://gist.github.com/edhiley/5da612c93e31c7e60355#part-1---postcode-validation
+     */
     public static boolean isPostCode(CharSequence text) {
         if (text == null)
             return false;
--- src/test/java/com/postcode/PostCodeValidatorTest.java
+++ src/test/java/com/postcode/PostCodeValidatorTest.java
@@ -36,53 +36,84 @@ public class PostCodeValidatorTest {
 
     @Test
     public final void whenPostCodeWithNoSpaceIsSupplied() {
-        Assert.assertTrue(PostCodeValidator.isPostCode("LS44PL"));
+        Assert.assertFalse(PostCodeValidator.isPostCode("LS44PL"));
     }
 
+    /*
+     *  The letters QVX are not used in the first position.
+     */
     @Test
     public final void whenQInFirstPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("Q1A 9AA"));
     }
 
+    /*
+     *  The letters QVX are not used in the first position.
+     */
     @Test
     public final void whenVInFirstPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("V1A 9AA"));
     }
 
+    /*
+     *  The letters QVX are not used in the first position.
+     */
     @Test
     public final void whenXInFirstPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("X1A 9BB"));
     }
 
+    /*
+     *  The letters IJZ are not used in the second position.
+     */
     @Test
     public final void whenIInSecondPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("LI10 3QP"));
     }
 
+    /*
+     *  The letters IJZ are not used in the second position.
+     */
     @Test
     public final void whenJInSecondPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("LJ10 3QP"));
     }
 
+    /*
+     *  The letters IJZ are not used in the second position.
+     */
     @Test
     public final void whenZInSecondPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("LZ10 3QP"));
     }
 
+    /*
+     *  The only letters to appear in the third position are ABCDEFGHJKPSTUW when the structure
+     *  starts with A9A.
+     */
     @Test
     public final void whenQInThirdPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("A9Q 9AA"));
     }
 
+    /*
+     *  The only letters to appear in the fourth position are ABEHMNPRVWXY when the structure starts
+     *  with AA9A.
+     */
     @Test
     public final void whenCInFourthPositionIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("AA9C 9AA"));
     }
 
+    /*
+     *  Areas with a district '0' (zero): BL, BS, CM, CR, FY, HA, PR, SL, SS (BS is the only area to
+     *  have both a district 0 and a district 10).
+     */
     @Test
     public final void whenAreaWithOnlySingleDigitDistrictsIsSupplied() {
         Assert.assertFalse(PostCodeValidator.isPostCode("FY10 4PL"));
     }
+
 
     @Test
     public final void whenAreaWithOnlyDoubleDigitDistrictsIsSupplied() {
